bac9d4debc8df4d0a450f16eac23d0bb676eb1bf
nirmalravidas <nirmalravidas.email@gmail.com>
2025-11-18 22:50:34 +0530

    setup gmail mail sender

--- backend/mail/nodemailer.ts
+++ backend/mail/nodemailer.ts
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: process.env.SMTP_HOST,
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
+
 
 export default transporter;
 
